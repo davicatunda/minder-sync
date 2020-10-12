b39f461e3822dc67b23f0214f71c4116f251500c
Davi Catunda <davicatunda@gmail.com>
2020-10-11 17:52:53 -0700

    quick fix

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ const MyDataTypes = {
   },
 };
 
-// User 
+/**
+ * User
+ */
 const UserTable = db.define('User', {
   uuid: MyDataTypes.TablePrimaryKey,
   username: DataTypes.STRING,
@@ -33,8 +35,41 @@ const UserGraphQLTypeDefinition = `
     token: String
   }
 `;
+const UserRootField = {
+  definition: 'user: User',
+  resolver: {
+    user: (_, __, { userId }) => UserTable.findOne({ where: { uuid: userId } }),
+  },
+};
 
-// Standard Proposal
+const LoginMutation = {
+  defition: 'login(username: String!, password: String!): String',
+  resolver: {
+    login: async (_, { username, password }) => {
+      const user = await UserTable.findOne({ where: { username } });
+      if (!user || !bcrypt.compareSync(password, user.password)) {
+        return null;
+      }
+      const token = jwt.sign({ id: user.uuid }, process.env.JWT_SECRET);
+      await user.update({ token });
+      return { token };
+    },
+  },
+};
+
+const LogoutMutation = {
+  definition: 'logout: Boolean',
+  resolver: {
+    logout: async (_, __, { userId }) => {
+      await UserTable.update({ where: { userId } }, { token: null });
+      return true;
+    }
+  },
+};
+
+/**
+ * Standard Proposal
+ */
 const StandardProposalTable = db.define('StandardProposal', {
   uuid: MyDataTypes.TablePrimaryKey,
   version: DataTypes.STRING,
@@ -48,7 +83,19 @@ const StandardProposalGraphQLTypeDefinition = `
   }
 `;
 
-// Proposal
+const StandardProposalRootField = {
+  defintion: `standardProposal: StandardProposal`,
+  resolver: {
+    standardProposal: () => StandardProposalTable.findAll({
+      limit: 1,
+      order: [['createdAt', 'DESC']]
+    }),
+  }
+}
+
+/**
+ * Proposal
+ */
 const ProposalTable = db.define('Proposal', {
   uuid: MyDataTypes.TablePrimaryKey,
   data: DataTypes.TEXT,
@@ -59,7 +106,29 @@ const ProposalGraphQLTypeDefinition = `
     data: String
   }
 `;
+const ProposalRootField = {
+  definition: 'proposal(uuid: String!): Proposal',
+  resolver: {
+    proposal: (_, { uuid }) => ProposalTable.findOne({ where: { uuid } }),
+  },
+};
 
+const ProposalsRootField = {
+  definition: 'proposals: [Proposal]',
+  resolver: {
+    proposals: () => ProposalTable.findAll(),
+  }
+};
+
+const AddProposalMutation = {
+  definition: 'addProposal(proposal: String): Proposal',
+  resolver: {
+    addProposal: async (_, { proposal }) => {
+      const newProposal = await ProposalTable.create({ data: proposal });
+      return { uuid: newProposal.uuid, data: proposal };
+    }
+  },
+};
 
 async function createContext(req) {
   const token = req.headers ? req.headers.authorization : null;
@@ -83,47 +152,28 @@ express()
           ${ProposalGraphQLTypeDefinition}
           ${StandardProposalGraphQLTypeDefinition}
           type Query {
-            proposal(uuid: String!): Proposal
-            proposals: [Proposal]
-            standardProposal: StandardProposal
-            user: User
+            ${ProposalRootField.definition}
+            ${ProposalsRootField.definition}
+            ${StandardProposalRootField.defintion}
+            ${UserRootField.definition}
           }
           type Mutation {
-            addProposal(proposal: String): Proposal
-            login(username: String!, password: String!): String
-            logout: Boolean
+            ${AddProposalMutation.definition}
+            ${LoginMutation.definition}
+            ${LogoutMutation.definition}
           }
         `,
         resolvers: {
           Query: {
-            proposal: (_, { uuid }) => ProposalTable.findOne({ where: { uuid } }),
-            proposals: () => ProposalTable.findAll(),
-            standardProposal: () => StandardProposalTable.findAll({
-              limit: 1,
-              order: [['createdAt', 'DESC']]
-            }),
-            user: (_, __, { userId }) => UserTable.findOne({ where: { uuid: userId } }),
+            ...ProposalRootField.resolver,
+            ...ProposalsRootField.resolver,
+            ...StandardProposalRootField.resolver,
+            ...UserRootField.resolver,
           },
           Mutation: {
-            addProposal: async (_, { proposal }) => {
-              const newProposal = await ProposalTable.create({ proposal });
-              return { uuid: newProposal.uuid, data: proposal };
-            },
-
-            login: async (_, { username, password }) => {
-              const user = await UserTable.findOne({ where: { username } });
-              if (!user || !bcrypt.compareSync(password, user.password)) {
-                return null;
-              }
-              const token = jwt.sign({ id: user.uuid }, process.env.JWT_SECRET);
-              await user.update({ token });
-              return { token };
-            },
-
-            logout: async (_, __, { userId }) => {
-              await UserTable.update({ where: { userId } }, { token: null });
-              return true;
-            }
+            ...AddProposalMutation.resolver,
+            ...LoginMutation.resolver,
+            ...LogoutMutation.resolver,
           },
         },
       }),
